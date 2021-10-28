a505f25f8f7b6542bba8cb3864510f9ee886adf1
A01379363 <a01379363@itesm.mx>
2021-10-28 11:08:43 -0500

    add yellow color

--- paint.py
+++ paint.py
@@ -8,6 +8,7 @@ Exercises
 4. Complete triangle.
 5. Add width parameter.
 
+Henry Mondelli
 """
 
 from turtle import *
@@ -89,6 +90,7 @@ onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
+onkey(lambda: color('red'), 'R')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
